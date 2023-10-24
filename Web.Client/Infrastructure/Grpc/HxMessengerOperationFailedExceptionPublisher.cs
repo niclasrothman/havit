3b947cb5c400c518c915e9a98d49a9a16ecf7be1
Niclas Rothman <80453282+niclasrothman@users.noreply.github.com>
2023-10-24 08:15:26 +0200

    Initial commit

---
+++ Web.Client/Infrastructure/Grpc/HxMessengerOperationFailedExceptionPublisher.cs
@@ -0,0 +1,24 @@
+﻿using Havit.Blazor.Grpc.Client.ServerExceptions;
+using Havit.NewProjectTemplate.Resources;
+using Microsoft.Extensions.Localization;
+
+namespace Havit.NewProjectTemplate.Web.Client.Infrastructure.Grpc;
+
+public class HxMessengerOperationFailedExceptionGrpcClientListener : IOperationFailedExceptionGrpcClientListener
+{
+	private readonly IHxMessengerService _messenger;
+	private readonly IStringLocalizer<Global> _localizer;
+
+	public HxMessengerOperationFailedExceptionGrpcClientListener(IHxMessengerService messenger, IStringLocalizer<Global> localizer)
+	{
+		this._messenger = messenger;
+		this._localizer = localizer;
+	}
+
+	public Task ProcessAsync(string errorMessage)
+	{
+		_messenger.AddError(_localizer["OperationFailedExceptionMessengerTitle"], errorMessage);
+
+		return Task.CompletedTask;
+	}
+}
